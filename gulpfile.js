59b060caa313b6b6d0adf573e90f75d854444d2f
ptflp <filippov.pa@s-vfu.ru>
2018-06-14 18:38:49 +0900

    added gulp autoprefixer rename cleancss

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,20 @@
 var
 	gulp 		= require("gulp"),
 	browserSync = require('browser-sync').create(),
+	autoprefixer= require('gulp-autoprefixer'),
+	cleancss	= require('gulp-cleancss'),
+	rename		= require('gulp-rename'),
 	sass		= require("gulp-sass");
 
 gulp.task("reload-css", function () {
 	gulp.src('./src/*.scss')
 	.pipe(sass().on('error',sass.logError))
+	.pipe(autoprefixer({
+		browsers: ['last 3 versions'],
+		cascade: false
+	}))
+	.pipe(cleancss({compatibility: 'ie8'}))
+	.pipe(rename({suffix: '.min'}))
 	.pipe(gulp.dest('./src/css/'))
     .pipe(browserSync.stream());
 });
